012edf0d99858031f81feb30b0260d3b74d069c9
Stephen Moore <stephen@delfick.com>
2025-04-25 08:26:28 +1000

    Implementing the QueueFeeder

--- machinery/_helpers/_queue_feeder.py
+++ machinery/_helpers/_queue_feeder.py
@@ -1,7 +1,8 @@
 import asyncio
 import dataclasses
 import enum
-from collections.abc import AsyncGenerator, Callable, Coroutine, Iterator, Sequence
+import inspect
+from collections.abc import AsyncGenerator, Callable, Coroutine, Generator, Iterator, Sequence
 from typing import Optional
 
 from . import _context, _protocols, _queue, _task_holder
@@ -78,59 +79,277 @@ class _QueueFeeder[T_QueueContext, T_Tramp: _protocols.Tramp = _protocols.Tramp]
         self.ctx = ctx
         self.queue = queue
         self.task_holder = task_holder
+        self.sent_stop = asyncio.Event()
         self.sources: list[_QueueSource] = []
         self.make_empty_context = make_empty_context
+        self.finished_if_empty_sources: bool = False
+
+        self.queue.ctx.add_done_callback(self._on_queue_stopped)
 
     def set_as_finished_if_out_of_sources(self) -> None:
-        pass
+        self.finished_if_empty_sources = True
+        self._clear_sources()
+
+    def _extend_result(
+        self, *, result: object, source: _QueueSource, context: T_QueueContext | None
+    ) -> None:
+        match result:
+            case Coroutine():
+                self.add_coroutine(result, context=context, _parent_source=source)
+            case asyncio.Task():
+                self.add_task(result, context=context, _parent_source=source)
+            case Generator():
+                self.add_sync_iterator(result, context=context, _parent_source=source)
+            case AsyncGenerator():
+                self.add_async_generator(result, context=context, _parent_source=source)
+            case _ if callable(result) and len(inspect.signature(result).parameters) == 0:
+                self.add_sync_function(result, context=context, _parent_source=source)
+            case _:
+                self.queue.append(
+                    QueueManagerSuccess(
+                        sources=source.sources,
+                        value=result,
+                        context=context if context is not None else self.make_empty_context(),
+                    )
+                )
 
     def add_sync_function(
         self,
         func: Callable[[], object],
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        source = _QueueSource(input_type=QueueInput.SYNC_FUNCTION, source=func)
+        self.sources.append(source)
+
+        try:
+            result = func()
+        except Exception as exc:
+            self.queue.append(
+                QueueManagerFailure(
+                    sources=source.sources,
+                    exception=exc,
+                    context=context if context is not None else self.make_empty_context(),
+                )
+            )
+        else:
+            self._extend_result(result=result, source=source, context=context)
+
+        source.finished = True
+        self._clear_sources()
 
     def add_sync_iterator(
         self,
         iterator: Iterator[object],
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        if isinstance(iterator, Generator):
+            source = _QueueSource(
+                input_type=QueueInput.SYNC_GENERATOR, source=iterator, parent_source=_parent_source
+            )
+        else:
+            source = _QueueSource(
+                input_type=QueueInput.SYNC_ITERATOR, source=iterator, parent_source=_parent_source
+            )
+
+        self.sources.append(source)
+
+        async def process_iterator() -> None:
+            while True:
+                try:
+                    nxt = next(iterator)
+                except StopIteration:
+                    break
+                except Exception as exc:
+                    event = asyncio.Event()
+                    self.ctx.loop.call_soon(event.set)
+                    await event.wait()
+                    self.queue.append(
+                        QueueManagerFailure(
+                            sources=source.sources,
+                            exception=exc,
+                            context=context if context is not None else self.make_empty_context(),
+                        )
+                    )
+                    break
+                else:
+                    event = asyncio.Event()
+                    self.ctx.loop.call_soon(event.set)
+                    await event.wait()
+                    self._extend_result(result=nxt, source=source, context=context)
+
+        def on_done(res: _protocols.FutureStatus[None]) -> None:
+            source.finished = True
+
+            exc: BaseException | None
+            if res.cancelled():
+                exc = asyncio.CancelledError()
+            else:
+                exc = res.exception()
+
+            self.queue.append(
+                QueueManagerIterationStop(
+                    sources=source.sources,
+                    exception=exc,
+                    context=context if context is not None else self.make_empty_context(),
+                )
+            )
+            source.finished = True
+            self._clear_sources()
+
+        task = self.task_holder.add(process_iterator())
+        task.add_done_callback(on_done)
+        self._clear_sources()
 
     def add_value(
         self,
         value: object,
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        source = _QueueSource(
+            input_type=QueueInput.VALUE, source=value, parent_source=_parent_source
+        )
+        self.sources.append(source)
+
+        self._extend_result(result=value, source=source, context=context)
+        source.finished = True
+        self._clear_sources()
 
     def add_coroutine(
         self,
         coro: Coroutine[object, object, object],
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        source = _QueueSource(
+            input_type=QueueInput.COROUTINE, source=coro, parent_source=_parent_source
+        )
+        self.sources.append(source)
+        self.add_task(self.task_holder.add(coro), context=context, _parent_source=source)
+
+        source.finished = True
+        self._clear_sources()
 
     def add_task(
         self,
         task: asyncio.Task[object],
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        source = _QueueSource(
+            input_type=QueueInput.TASK, source=task, parent_source=_parent_source
+        )
+        self.sources.append(source)
+
+        def on_done(res: _protocols.FutureStatus[object]) -> None:
+            source.finished = True
+
+            exc: BaseException | None
+            if res.cancelled():
+                exc = asyncio.CancelledError()
+            else:
+                exc = res.exception()
+
+            if exc is not None:
+                self.queue.append(
+                    QueueManagerFailure(
+                        sources=source.sources,
+                        exception=exc,
+                        context=context if context is not None else self.make_empty_context(),
+                    )
+                )
+            else:
+                self._extend_result(result=res.result(), source=source, context=context)
+
+            source.finished = True
+            self._clear_sources()
+
+        if task.done():
+            on_done(task)
+        else:
+            task.add_done_callback(on_done)
 
     def add_async_generator(
         self,
         agen: AsyncGenerator[object],
         *,
         context: T_QueueContext | None = None,
+        _parent_source: _QueueSource | None = None,
     ) -> None:
-        pass
+        source = _QueueSource(
+            input_type=QueueInput.ASYNC_GENERATOR, source=agen, parent_source=_parent_source
+        )
+        self.sources.append(source)
+
+        async def process_generator() -> None:
+            while True:
+                try:
+                    nxt = await agen.__anext__()
+                except StopAsyncIteration:
+                    break
+                except Exception as exc:
+                    self.queue.append(
+                        QueueManagerFailure(
+                            sources=source.sources,
+                            exception=exc,
+                            context=context if context is not None else self.make_empty_context(),
+                        )
+                    )
+                    break
+                else:
+                    self._extend_result(result=nxt, source=source, context=context)
+
+        def on_done(res: _protocols.FutureStatus[None]) -> None:
+            source.finished = True
+
+            exc: BaseException | None
+            if res.cancelled():
+                exc = asyncio.CancelledError()
+            else:
+                exc = res.exception()
+
+            self.queue.append(
+                QueueManagerIterationStop(
+                    sources=source.sources,
+                    exception=exc,
+                    context=context if context is not None else self.make_empty_context(),
+                )
+            )
+            source.finished = True
+            self._clear_sources()
+
+        task = self.task_holder.add(process_generator())
+        task.add_done_callback(on_done)
+        self._clear_sources()
+
+    def _on_queue_stopped(self, res: _protocols.FutureStatus[None]) -> None:
+        exc: BaseException | None
+        if res.cancelled():
+            exc = asyncio.CancelledError()
+        else:
+            exc = res.exception()
+
+        self._send_stop(exc)
+
+    def _clear_sources(self) -> None:
+        self.sources = [source for source in self.sources if not source.finished]
+        if not self.sources and self.finished_if_empty_sources:
+            self._send_stop()
+
+    def _send_stop(self, exc: BaseException | None = None, /) -> None:
+        if self.sent_stop.is_set():
+            return
+
+        self.sent_stop.set()
+        self.queue.append(QueueManagerStopped(exception=exc))
+        self.queue.ctx.cancel()
 
 
 class QueueManager[T_QueueContext, T_Tramp: _protocols.Tramp = _protocols.Tramp]:
