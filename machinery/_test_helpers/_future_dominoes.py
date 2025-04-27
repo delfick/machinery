27e91561fd312425c57e59949d35a6a8a4923c60
Stephen Moore <stephen@delfick.com>
2025-04-27 15:34:19 +1000

    Add docs for FutureDominoes

--- machinery/_test_helpers/_future_dominoes.py
+++ machinery/_test_helpers/_future_dominoes.py
@@ -11,41 +11,93 @@ from machinery import helpers as hp
 
 
 class Domino(Protocol):
-    def __await__(self) -> Generator[None]: ...
+    """
+    Used to represent the futures provided by FutureDominoes.
+    """
+
+    def __await__(self) -> Generator[None]:
+        """
+        Wait for the domino to be knocked over.
+        """
 
     def add_done_callback(
         self, cb: Callable[[hp.protocols.FutureStatus[None]], None]
-    ) -> hp.protocols.FutureCallback[None]: ...
+    ) -> hp.protocols.FutureCallback[None]:
+        """
+        Add a callback for when the domino is knocked over
+        """
 
-    def done(self) -> bool: ...
+    def done(self) -> bool:
+        """
+        Return true if the domino has been knocked over
+        """
 
-    def cancelled(self) -> bool: ...
+    def cancelled(self) -> bool:
+        """
+        Return true if the domino was cancelled
+        """
 
-    def exception(self) -> BaseException | None: ...
+    def exception(self) -> BaseException | None:
+        """
+        Return true if the domino was given an exception
+        """
 
-    def result(self) -> None: ...
+    def result(self) -> None:
+        """
+        Return the result from the domino
+        """
 
-    def set_result(self, value: None) -> None: ...
+    def set_result(self, value: None) -> None:
+        """
+        Knock over a domino with no exception
+        """
 
-    def set_exception(self, exc: BaseException) -> None: ...
+    def set_exception(self, exc: BaseException) -> None:
+        """
+        Set an exception on the domino
+        """
 
-    def cancel(self) -> None: ...
+    def cancel(self) -> None:
+        """
+        Set the domino as cancelled (give it an exception of asyncio.CancelledError)
+        """
 
 
 class FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp](Protocol):
-    @property
-    def started(self) -> asyncio.Event: ...
+    """
+    An object that represents a "domino" set of futures that only complete as
+    each previous domino is retrieved and awaited
+    """
 
     @property
-    def finished(self) -> asyncio.Event: ...
+    def started(self) -> asyncio.Event:
+        """
+        This is set for us when the dominoes have started
+        """
 
-    def begin(self) -> None: ...
+    @property
+    def finished(self) -> asyncio.Event:
+        """
+        This is set for us when all the dominoes have been knocked over
+        """
 
-    def __getitem__(self, num: int) -> Domino: ...
+    def begin(self) -> None:
+        """
+        Used by the test to indicate that the dominoes should begin
+        """
+
+    def __getitem__(self, num: int) -> Domino:
+        """
+        Get the domino indexed by ``num``
+        """
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class _Domino[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
+    """
+    Implementation for each domino
+    """
+
     _i: int
     _ctx: hp.CTX[T_Tramp]
     _fut: asyncio.Future[None]
@@ -53,9 +105,18 @@ class _Domino[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
     _requirements: Sequence[tuple[asyncio.Future[None], asyncio.Future[None]]]
 
     def __await__(self) -> Generator[None]:
-        return (yield from self._wait().__await__())
+        """
+        Wait for the domino to be complete
+        """
+        return self._wait().__await__()
 
     async def _wait(self) -> None:
+        """
+        Implementation for __await__
+
+        We wait for the dominos to begin and then wait for each proceeding
+        domino to be retrieved and knocked over.
+        """
         await self._started.wait()
 
         for retrieved, fut in self._requirements:
@@ -64,33 +125,65 @@ class _Domino[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
     def add_done_callback(
         self, cb: Callable[[hp.protocols.FutureStatus[None]], None]
     ) -> hp.protocols.FutureCallback[None]:
+        """
+        Add a callback to call when the domino is knocked over
+        """
         self._fut.add_done_callback(cb)
         return cb
 
     def done(self) -> bool:
+        """
+        Return True if the domino is knocked over.
+        """
         return self._fut.done()
 
     def cancelled(self) -> bool:
+        """
+        Return True if the domino was cancelled.
+        """
         return self._fut.cancelled()
 
     def exception(self) -> BaseException | None:
+        """
+        Return the exception the future was given if one was
+
+        Raises an exception if the domino isn't knocked over yet
+        """
         return self._fut.exception()
 
     def result(self) -> None:
+        """
+        Return the result the future was given.
+
+        Raises an exception if the domino wasn't completed with a result.
+        """
         return self._fut.result()
 
     def set_result(self, value: None) -> None:
+        """
+        Knock over the domino with no exception
+        """
         self._fut.set_result(value)
 
     def set_exception(self, exc: BaseException) -> None:
+        """
+        Set an exception on the domino
+        """
         self._fut.set_exception(exc)
 
     def cancel(self) -> None:
+        """
+        Set the domino as cancelled (give it an exception of asyncio.CancelledError)
+        """
         self._fut.cancel()
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
+    """
+    Implementation for our future dominoes.
+    """
+
     _ctx: hp.CTX[T_Tramp]
 
     started: asyncio.Event
@@ -101,6 +194,9 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
     _retrieved: dict[int, asyncio.Future[None]]
 
     def __enter__(self) -> Self:
+        """
+        Nothing to do on entering the context manager
+        """
         return self
 
     def __exit__(
@@ -109,6 +205,10 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
         value: BaseException | None,
         tb: types.TracebackType | None,
     ) -> None:
+        """
+        Ensure all futures were retrieved and awaited after leaving the
+        context manager.
+        """
         self.check_finished()
 
     @classmethod
@@ -119,6 +219,11 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
         task_holder: hp.protocols.TaskHolder,
         expected: int,
     ) -> Self:
+        """
+        A classmethod constructor for correctly creating one of these objects.
+
+        We set up all the dominos that will then be managed.
+        """
         futs: dict[int, Domino] = {}
         retrieved: dict[int, asyncio.Future[None]] = {}
 
@@ -127,6 +232,9 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
 
         requirements: list[tuple[asyncio.Future[None], asyncio.Future[None]]] = []
 
+        # We make sure that when the dominoes are retrieved the first time that
+        # is announced.
+        # We also create each domino and provide that to our list of futures
         for i in range(1, expected + 1):
             retrieved_fut = ctx.loop.create_future()
             ctx.tramp.set_future_name(retrieved_fut, name=f"{ctx.name}-->[Retrieved({i})]")
@@ -160,9 +268,23 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
         def finished_on_ctx_done(res: hp.protocols.FutureStatus[None]) -> None:
             instance.finished.set()
 
+        # Make sure we set finished when we are done
         ctx.add_done_callback(finished_on_ctx_done)
 
         async def knock() -> None:
+            """
+            This is what ends up knocking over the dominoes.
+
+            Essentially we create a background task that waits for each domino
+            to be retrieved and awaited before we then set the next domino as
+            done.
+
+            This means that we don't knock over dominoes until they have actually
+            been retrieved.
+
+            Otherwise later dominoes may be marked as complete if they are
+            retrieved before earlier dominoes.
+            """
             await started.wait()
 
             for i, (retrieved, fut) in enumerate(requirements):
@@ -179,9 +301,19 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
         return instance
 
     def begin(self) -> None:
+        """
+        Mark the dominoes as ready to starting knocking over
+        """
         self.started.set()
 
     def check_finished(self) -> None:
+        """
+        Run when the context manager finishes, and ensures that the test
+        retrieved and awaited all the dominoes.
+
+        Otherwise the test may silently passed even if it didn't use up all
+        the dominoes!
+        """
         not_done: set[int] = set()
         for i, fut in self._futs.items():
             if not fut.done():
@@ -199,11 +331,25 @@ class _FutureDominoes[T_Tramp: hp.protocols.Tramp = hp.protocols.Tramp]:
             raise AssertionError(f"Not all the futures were completed: {not_done}")
 
     def __getitem__(self, num: int) -> Domino:
+        """
+        Retrieve the domino for this number.
+
+        Note that dominoes are 1-indexed and so if we expect 4 dominoes then
+        that means we expect to retrieve and await ``futs[1]``, ``futs[2]``,
+        ``futs[3]`` and ``futs[4]``.
+        """
         if not self._retrieved[num].done():
             self._retrieved[num].set_result(None)
         return self._futs[num]
 
     async def _allow_real_loop(self) -> None:
+        """
+        We use this when we are knocking over dominoes to ensure that as each
+        domino is knocked over, anything else on the loop is given time to
+        execute before we knock over the next domino.
+
+        Otherwise things can get a little less deterministic.
+        """
         while (
             len(
                 self._ctx.loop._ready  # type: ignore[attr-defined]
